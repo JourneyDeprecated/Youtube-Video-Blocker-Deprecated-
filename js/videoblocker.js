d7ca9ae06fec67e9f5238a47233af487796c090d
Journey <timtag1190@gmail.com>
2017-04-23 08:02:34 -0500

    Clarify better on why "via right click on a video thumbnail" may not work.

--- js/videoblocker.js
+++ js/videoblocker.js
@@ -104,7 +104,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
         '</ul>' +
         '<p>More information can be found on the settings page under the \'Help\' section.</p>' +
         '<hr>' +
-        '<p>*"via right click on a video thumbnail" currently is a wip as it does not work right now.</p>' +
+        '<p>*"via right click on a video thumbnail" other extensions and/or tampermonkey/greasemonkey scripts may or may not break this functionality.</p>' +
         '<hr>' +
         '<p style="text-align:center; margin-bottom:0; font-weight:500;"><a id="videoblocker-closewindow" href="#">Close this window (until the next update)</a></p>';
       inner.appendChild(content);
